be9f1ebd09dc86fd61be62f8c6998fd533590b09
FrostySo <24198637+FrostySo@users.noreply.github.com>
2023-08-06 20:33:48 +1200

    other stuff

---
+++ Ps4-Pkg-Sender/Controls/Theming/Options/OptionsSample.cs
@@ -0,0 +1,16 @@
+﻿using Ps4_Pkg_Sender.Controls.Theming.Options.Interfaces;
+using Ps4_Pkg_Sender.Controls.Theming.Options.Setting;
+using System.Drawing;
+
+namespace Ps4_Pkg_Sender.Controls.Theming.Options {
+    public class OptionsSample {
+        protected RenderOption RenderOption { get; set; }
+
+        private IControlSample _controlSample;
+
+        public OptionsSample(IControlSample controlSample) {
+            this._controlSample = controlSample;
+        }
+
+    }
+}
--- Ps4-Pkg-Sender/Controls/Theming/Options/RenderOption.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Ps4_Pkg_Sender.Controls.Theming.Options {
-    public enum RenderOption {
-        Paint,
-        Control
-    }
-}
